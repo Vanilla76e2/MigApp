fa1b7e91d017f37b7d07820acaf26220ff3612fc
Vanilla76e2 <nikitaposto3@gmail.com>
2024-05-15 20:22:31 +0500

    feat: начальная рабочая версия MigApp
    - Добавлены основные окна и логика приложения (Login, MainWindow, админ-панели)
    - Реализованы утилиты подключения к базе данных
    - Добавлены начальные ресурсы UI и изображения
    - Включено руководство пользователя и справочная документация

---
+++ VisualStudio/MigApp/CRWindows/AdminPanel/UsersWindow.xaml.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MigApp.CRWindows.AdminPanel
+{
+    /// <summary>
+    /// Логика взаимодействия для UsersWindow.xaml
+    /// </summary>
+    public partial class UsersWindow : Window
+    {
+        SQLConnectionClass sqlcc = new SQLConnectionClass();
+        MiscClass mc = new MiscClass();
+        DataTable table = new DataTable();
+        string CurrentUser = MigApp.Properties.Settings.Default.UserLogin;
+        string ID;
+
+        // true - Создание
+        // false - Редактирование
+        bool Mode;
+
+        public UsersWindow(bool mode, string id)
+        {
+            InitializeComponent();
+            Mode = mode;
+            ID = id;
+            Start(id);
+        }
+
+        // Нажатие кнопки "Сохранить"
+        private void SaveClick(object sender, RoutedEventArgs e)
+        {
+            if (Login.Text.Length > 0 && Employee.Text.Length > 0 && Role.Text.Length > 0)
+            {
+                if (Convert.ToInt32(sqlcc.ReqRef($"SELECT COUNT(*) FROM Users Where Login LIKE '{Login.Text}'")) < 1)
+                {
+                    if (Mode)
+                    {
+                        sqlcc.ReqNonRef($"INSERT INTO Users (Login, Employee, Role) Values ('{Login.Text}', (SELECT ID FROM Employees WHERE FIO LIKE '{Employee.Text}'), (SELECT ID FROM Roles WHERE Name LIKE '{Role.Text}'))");
+                        sqlcc.Loging(CurrentUser, "Создание", "Пользователи", Login.Text, "");
+                    }
+                    else
+                    {
+                        sqlcc.ReqNonRef($"UPDATE Users SET Login = '{Login.Text}', Employee = (SELECT ID FROM Employees WHERE FIO LIKE '{Employee.Text}'), Role = (SELECT ID FROM Roles WHERE Name LIKE '{Role.Text}')");
+                        sqlcc.Loging(CurrentUser, "Редактирование", "Пользователи", Login.Text, "");
+                    }
+                    DialogResult = true; Close();
+                }
+                else
+                {
+                    Login.Focus();
+                    MessageBox.Show("Такой пользователь уже существует.", "Внимание", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Не все поля заполнены!", "Внимание!", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        // Нажатие кнопки "Удалить"
+        private void DeleteClick(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show($"Вы уверены что хотите удалить пользователя '{Login.Text}'?\nПользователь будет безвозвратно удалён!", "Внимание", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+            {
+                sqlcc.ReqDel($"DELETE FROM Users WHERE Login LIKE '{Login.Text}'");
+                DialogResult = true; Close();
+            }
+        }
+
+        // Кнопка сброса пароля
+        private void ResetPassword(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show($"Вы уверены что хотите сбросить пароль пользователя '{Login.Text}'?", "Внимание", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+            {
+                sqlcc.ReqNonRef($"UPDATE Useres SET Password = NULL WHERE Login LIKE '{Login.Text}'");
+                sqlcc.Loging(CurrentUser, "Сброс пароля", "Пользователи", Login.Text, "");
+            }
+        }
+
+        #region User ComboBox
+
+        private void EmployeeListFill()
+        {
+            DataTable table = new DataTable();
+            table = sqlcc.DataGridUpdate("FIO", "Employees", "WHERE Deleted = 0");
+            foreach (DataRow row in table.Rows) 
+            {
+                userList.Add(row["FIO"].ToString());
+            }
+            Employee.ItemsSource = userList;
+        }
+
+        private void RoleListFill()
+        {
+            DataTable table = new DataTable();
+            table = sqlcc.DataGridUpdate("Name", "Roles", "");
+            foreach(DataRow row in table.Rows)
+            {
+                roleList.Add(row["Name"].ToString());
+            }
+            Role.ItemsSource = roleList;
+        }
+
+        private List<string> userList = new List<string>();
+        private List<string> roleList = new List<string>();
+
+        private void UserChanged(object sender, TextChangedEventArgs e)
+        {
+            Employee.IsDropDownOpen = true;
+            Employee.SelectedIndex = -1;
+            string temp = ((ComboBox)sender).Text;
+            var newList = userList.Where(x => x.Contains(temp));
+            Employee.ItemsSource = newList.ToList();
+        }
+
+        #endregion
+
+        // Заполнение полей и изменение названия окна
+        private void Start(string ID)
+        {
+            EmployeeListFill();
+            RoleListFill();
+            if (Mode)
+            {
+                Title = "Пользователи (Создание)";
+                DeleteButton.Visibility = Visibility.Collapsed;
+                ResetPasswordButton.IsEnabled = false;
+            }
+            else
+            {
+                Login.IsReadOnly = true;
+                Title = "Пользователи (Редактирование)";
+                table = sqlcc.DataGridUpdate("*", "Users_View", $"Where ID Like '{ID}'");
+                Login.Text = table.Rows[0].Field<string>("Логин");
+                Employee.Text = table.Rows[0].Field<string>("Сотрудник");
+                Role.Text = table.Rows[0].Field<string>("Роль");
+                if (sqlcc.ReqRef($"SELECT Password FROM Users WHERE Login LIKE '{Login.Text}'") == "")
+                    ResetPasswordButton.IsEnabled = false;
+            }
+        }
+
+        
+    }
+}
---
+++ VisualStudio/MigApp/CRWindows/EmployeesWindow.xaml.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MigApp.CRWindows
+{
+    public partial class EmployeesWindow : Window
+    {
+        SQLConnectionClass sqlcc = new SQLConnectionClass();
+        MiscClass mc = new MiscClass();
+        DataTable table = new DataTable();
+        string CurrentUser = MigApp.Properties.Settings.Default.UserLogin;
+        string ID;
+        bool Deleted;
+
+        // true - Создание
+        // false - Редактирование
+        bool Mode;
+
+        public EmployeesWindow(bool mode, string id, bool deleted)
+        {
+            InitializeComponent();
+            Mode = mode;
+            ID = id;
+            Deleted = deleted;
+            Start(id);
+        }
+
+        // Нажатие кнопки "Сохранить"
+        private void SaveClick(object sender, RoutedEventArgs e)
+        {
+            if (FIO.Text.Length > 0 && Group.Text.Length > 0 && Room.Text.Length > 0 && BirthDate.Text.Length > 0)
+            {
+                if (Convert.ToInt32(sqlcc.ReqRef($"SELECT COUNT(*) FROM Employees Where FIO LIKE '{FIO.Text}'")) < 1 || !Mode)
+                {
+                    if (Mode)
+                    {
+                        sqlcc.ReqNonRef($"INSERT INTO Employees (FIO, [Group], Room, Birthdate) Values ('{FIO.Text}', '{Group.Text}', '{Room.Text}', '{BirthDate.Text}')");
+                        sqlcc.Loging(CurrentUser, "Создание", "Сотрудники", FIO.Text, "");
+                    }
+                    else
+                    {
+                        sqlcc.ReqNonRef($"UPDATE Employees SET FIO = '{FIO.Text}', [Group] = '{Group.Text}', Room = '{Room.Text}', Birthdate = '{BirthDate.Text}' WHERE ID LIKE {ID}");
+                        sqlcc.Loging(CurrentUser, "Редактирование", "Сотрудники", FIO.Text, "");
+                    }
+                    DialogResult = true; Close();
+                }
+                else
+                {
+                    FIO.Focus();
+                    MessageBox.Show("Такой сотрудник уже существует.", "Внимание", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Не все поля заполнены.", "Внимание", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        // Нажатие кнопки "Удалить"
+        private void DeleteClick(object sender, RoutedEventArgs e)
+        {
+            if (!Deleted)
+            {
+                if (MessageBox.Show("Вы уверены что хотите удалить запись?","Внимание", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+                {
+                    sqlcc.ReqNonRef($"UPDATE Employees SET Deleted = 1, DelDate = '{DateTime.Now}' WHERE ID LIKE {ID}");
+                    sqlcc.Loging(CurrentUser, "Удаление", "Сотрудники", FIO.Text, "");
+                    DialogResult = true; Close();
+                }
+            }
+            else
+            {
+                if (MessageBox.Show("Запись будет безвозвратно удалена.\nХотите удалить запись?", "Внимание", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+                {
+                    sqlcc.Delete_DeletedEmployee(ID);
+                    sqlcc.Loging(CurrentUser, "Стирание", "Сотрудники", FIO.Text, "");
+                    DialogResult = true; Close();
+                }
+
+            }
+        }
+
+        // Нажатие кнопки "Восстановить"
+        private void RecoveryClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                sqlcc.Recovery("Employees", "ID", ID);
+                sqlcc.Loging(CurrentUser, "Восстановление", "Сотрудники", FIO.Text, "");
+                DialogResult = true; Close();
+            }
+            catch { }
+        }
+
+        // Проверка даты
+        private void DateCheck(object sender, SelectionChangedEventArgs e)
+        {
+            if (BirthDate.SelectedDate > DateTime.Now.AddYears(-14))
+            {
+                MessageBox.Show("Сотрудник не может быть младше 14 лет", "Внимание", MessageBoxButton.OK, MessageBoxImage.Warning);
+                BirthDate.SelectedDate = null;
+            }
+        }
+
+        // Заполнение полей и изменение названия окна
+        private void Start(string ID)
+        {
+            if (Mode)
+            {
+                Title = "Сотрудники (Создание)";
+                DeleteButton.Visibility = Visibility.Collapsed;
+            }
+            else if (!Mode && !Deleted)
+            {
+                try
+                {
+                    Title = "Сотрудники (Редактирование)";
+                    table = sqlcc.DataGridUpdate("*", "Employees_View", $"WHERE ID LIKE {ID}");
+                    DataRow row = table.Rows[0];
+                    FIO.Text = row["ФИО"].ToString();
+                    Group.Text = row["Отдел"].ToString();
+                    Room.Text = row["Кабинет"].ToString();
+                    BirthDate.Text = row["Дата рождения"].ToString();
+                }
+                catch
+                { MessageBox.Show("Запись не найдена", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error); }
+            }
+            else if (!Mode && Deleted)
+            {
+                try
+                {
+                    LockAll();
+                    Title = "Сотрудники (Редактирование)";
+                    table = sqlcc.DataGridUpdate("*", "Employees_Deleted", $"WHERE ID LIKE {ID}");
+                    DataRow row = table.Rows[0];
+                    FIO.Text = row["ФИО"].ToString();
+                    Group.Text = row["Отдел"].ToString();
+                    Room.Text = row["Кабинет"].ToString();
+                    BirthDate.Text = row["Дата рождения"].ToString();
+                }
+                catch
+                { MessageBox.Show("Запись не найдена", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error); }
+            }
+        }
+
+        private void NumOnly(object sender, TextCompositionEventArgs e)
+        {
+            if (!Char.IsDigit(e.Text, 0))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void LockAll()
+        {
+            FIO.IsReadOnly = true;
+            Group.IsReadOnly = true;
+            Room.IsReadOnly = true;
+            BirthDate.IsEnabled = false;
+        }
+    }
+}
